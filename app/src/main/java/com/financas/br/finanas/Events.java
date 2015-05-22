c236024b10aa40d8e099c9c5261ab7f99bdd01a6
Lucas Alves <lucashalves@live.com>
2015-05-22 19:25:18 -0300

    first commit

---
+++ app/src/main/java/com/financas/br/finanas/Events.java
@@ -0,0 +1,25 @@
+package com.financas.br.finanas;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.content.Context;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.support.v4.widget.DrawerLayout;
+import android.widget.*;
+
+
+public class Events extends ActionBarActivity {
+
+
+}
